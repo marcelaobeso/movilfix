46eb355559d42cb1daafb642959bcb451c3f8b72
marcelaobeso <march.obeso@gmail.com>
2022-01-04 21:40:33 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./components/tarjetaProducto";
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Tarjetitas!</h1>
+    <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/tarjetaProducto.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card= () => {
+    return (
+        <div>
+            <h1>TARJETA</h1>
+            
+        </div>
+    )
+}
+
+export default Card
